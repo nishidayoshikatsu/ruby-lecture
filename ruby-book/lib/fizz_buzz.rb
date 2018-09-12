c8c00875d660e23786476479a710bbcc3cc00c1f
nishidayoshikatsu <nsd24technology@gmail.com>
2018-09-12 06:50:29 +0000

    modefied fizz_buzz.rb

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,14 +1,17 @@
 def fizz_buzz(num)
-    if(!num%15)
+    if num % 15 == 0
         puts("Fizz Buzz")
-    elsif(!num%5)
+    elsif num % 5 == 0
         puts("Buzz")
-    elsif(!num%3)
+    elsif num % 3 == 0
         puts("Fizz")
     else
         puts(num.to_s)
     end
 end
 
-a=15;b=5;c=3
-fizz_buzz(a)git
+a=15;b=5;c=3;d=11
+fizz_buzz(a)
+fizz_buzz(b)
+fizz_buzz(c)
+fizz_buzz(d)
